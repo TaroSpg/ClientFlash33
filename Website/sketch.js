d04436af029fbf9616d65083a9fefb5d0408cfca
TaroSpg <tspirig@fas.harvard.edu>
2022-05-11 14:48:10 -0400

    “new”

--- Website/sketch.js
+++ Website/sketch.js
@@ -92,16 +92,32 @@ if (newcolor.levels[0]!=col.levels[0]||newcolor.levels[1]!=col.levels[1]||newcol
   noStroke();
   ambientLight(0, 0, 0);
   pointLight(lerpColor(prevcolor, col, smoothstep(0.1,0.7,amt)), 0, 0, 600);
-  if (checkaccel>=25){
-      accposy=accelerationY;
-      accposx=accelerationX;
-      checkaccel=0;
-  }
-  //pointLight(lerpColor(prevcolor, col, smoothstep(0.1,0.7,amt)), accposy*200, accposz*200, 600);
-  pointLight(lerpColor(prevcolor, col, smoothstep(0.1,0.7,amt)), accposx*10, accposy*10, 510);
+  // if (checkaccel>=50){
+  //     accposy=accelerationY;
+  //     accposx=accelerationX;
+  //     //accposz=accelerationZ;
+  //     checkaccel=0;
+  // }
+  accposy=Math.sign(accelerationY)*(Math.abs(accelerationY+1.2)+Math.abs(accposy))/2;
+  accposx=Math.sign(accelerationX)*(Math.abs(accelerationX)+Math.abs(accposx))/2;
+  pointLight(lerpColor(prevcolor, col, smoothstep(0.1,0.7,amt)), accposx*150, accposy*150, 505);
+  console.log('accposx', accposx);
+  console.log('accposy', accposy);
   specularMaterial(250);
   shininess(5);
   sphere(500);
+
+  
+  // if (checkaccel>=25){
+  //     accposy=accelerationY;
+  //     accposx=accelerationX;
+  //     checkaccel=0;
+  // }
+  // //pointLight(lerpColor(prevcolor, col, smoothstep(0.1,0.7,amt)), accposy*200, accposz*200, 600);
+  // pointLight(lerpColor(prevcolor, col, smoothstep(0.1,0.7,amt)), accposx*10, accposy*10, 510);
+  // specularMaterial(250);
+  // shininess(5);
+  // sphere(500);
   
   //textSize(32);
   //text(gridId, 10, 30);
